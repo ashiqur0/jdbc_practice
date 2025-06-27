bf06279dd568d7b2d1cebca0bafee5f2c486b716
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-27 16:07:24 +0600

    JDBC: Java Database Connectivity

--- src/Retrieve.java
+++ src/Retrieve.java
@@ -1,20 +1,24 @@
+/*
+JDBC : Retrieve Data From MySql Database
+
+*/
+
 import java.sql.*;
 
-class b {
+public class Retrieve {
     static final String url = "jdbc:mysql://127.0.0.1/university";
-    static final String user = "root";
+    static final String user_name = "root";
     static final String password = "root";
-    
+
     public static void main(String[] args) throws Exception {
-        String query = "select * from university.students";
+        String query = "SELECT * FROM UNIVERSITY.STUDENTS";
 
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection connection = DriverManager.getConnection(url, user, password);
+        Connection connection = DriverManager.getConnection(url, user_name, password);
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery(query);
 
-        System.out.println("Roll\t" + "Name");
-        while (resultSet.next()) {
+        while(resultSet.next()) {
             System.out.println(resultSet.getInt(1) + "\t" + resultSet.getString(2));
         }
 
